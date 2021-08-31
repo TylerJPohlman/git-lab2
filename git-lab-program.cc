abfa8c44bb04d796e1a9ed7df2e7ef6ca1adc885
Tyler Pohlman <tp987320@ohio.edu>
2021-08-31 18:00:34 -0400

    My first program on git!

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tyler Pohlman
+ *        Date: 08/32/2021
+ * Description: Prints out "Hello Git!!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
